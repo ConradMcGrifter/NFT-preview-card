5e1377a964e1cee1a224ceb72995b6c3ac503ab4
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2022-02-01 23:20:01 -0500

    added javaScript to its own file and added a screenshot

---
+++ script.js
@@ -0,0 +1,19 @@
+const cards = document.querySelectorAll(".cardImg-wrap");
+const lightBox = document.querySelector(".lightbox");
+const lightBoxImg = document.querySelector(".lightbox__img");
+const lightBoxCloseBtn = document.querySelector(".lightbox__closeBtn");
+
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        // toggle the lightbox to display
+        lightBox.classList.toggle("--is-active");
+        // get the src attribute from the image of the card being clicked
+        let imgSrc = card.querySelector(".card__img").getAttribute("src");
+        // add the img src to the background-image url
+        lightBoxImg.style.backgroundImage = `url(${imgSrc})`;
+    });
+});
+// close lightbox when closeBtn is clicked
+lightBoxCloseBtn.addEventListener("click", () => {
+    lightBox.classList.toggle("--is-active");
+});
